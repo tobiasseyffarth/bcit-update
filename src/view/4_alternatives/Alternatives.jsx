1425d3a8105b9e3822d0354d94d998f11960a1d1
Tobias Seyffarth <tobias.seyffarth@student.uni-halle.de>
2019-04-25 16:54:19 +0200

    dialog create new compliance process

--- src/view/4_alternatives/Alternatives.jsx
+++ src/view/4_alternatives/Alternatives.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { Dialog } from 'primereact/dialog';
 import { ListBox } from 'primereact/listbox';
 import { Button } from 'primereact/button';
+import { Dropdown } from 'primereact/dropdown';
 import { Checkbox } from 'primereact/checkbox';
 import { Growl } from 'primereact/growl';
+import {InputText} from 'primereact/inputtext';
 import PropTypes from 'prop-types';
 import ProjectModel from '../../models/ProjectModel';
 import * as GraphRenderer from "../../controller/graph/GraphRenderer";
@@ -19,11 +21,18 @@ export default class Alternatives extends Component {
       modelType: null,
       nodeProps: [],
       selectedCompliance: null,
+      visibleCPDialog: false,
+      processName: '',
+      processRule: '',
+      processProps: []
     };
 
+    this.onHide= this.onHide.bind(this);
     this.addComplianceRequirement = this.addComplianceRequirement.bind(this);
-    this.addComplianceProcess = this.addComplianceProcess.bind(this);
-    this.addComplianceProcessPattern = this.addComplianceProcessPattern.bind(this);
+    this.addComplianceProcess= this.addComplianceProcess.bind(this);
+    // this.addComplianceProcessPattern= this.addComplianceProcessPattern.bind(this);
+    this.showComplianceProcessAddDialog = this.showComplianceProcessAddDialog.bind(this);
+    this.showComplianceProcessPatternAddDialog = this.showComplianceProcessPatternAddDialog.bind(this);
     this.removeNode = this.removeNode.bind(this);
   }
 
@@ -49,6 +58,10 @@ export default class Alternatives extends Component {
     GraphRenderer.resizeGraph(this.graph);
     GraphRenderer.colorNodes(this.graph);
     this.hookGraphOnClick(this.graph);
+  }
+
+  onHide() {
+    this.setState({ visibleCPDialog: false });
   }
 
   hookGraphOnClick(graph){
@@ -103,22 +116,28 @@ export default class Alternatives extends Component {
   }
 
   addComplianceProcess(){
+
+    this.onHide();
+  }
+
+
+  showComplianceProcessAddDialog(){
     if (this.state.nodeType === null) {
       this.growl.show({ severity: 'warn', summary: 'Please select an compliance requirement from the graph.', detail:'' });
     } else if (this.state.nodeType === 'complianceprocess') {
       this.growl.show({ severity: 'warn', summary: 'Please select an compliance requirement from the graph.', detail:'' });
     } else {
-
+      this.setState({ visibleCPDialog: true });
     }
   }
 
-  addComplianceProcessPattern(){
+  showComplianceProcessPatternAddDialog(){
     if (this.state.nodeType === null) {
       this.growl.show({ severity: 'warn', summary: 'Please select an compliance requirement from the graph.', detail:'' });
     } else if (this.state.nodeType !== 'compliance') {
       this.growl.show({ severity: 'warn', summary: 'Please select an compliance requirement from the graph.', detail:'' });
     } else {
-
+      this.setState({ visibleCPDialog: true });
     }
   }
 
@@ -136,6 +155,37 @@ export default class Alternatives extends Component {
     } else {
       this.growl.show({ severity: 'warn', summary: 'Please select a node from the graph.', detail:'' });
     }
+  }
+
+  renderCPDialog() {
+    const footer = (
+      <div>
+        <Button label="Add" onClick={this.addComplianceProcess} />
+        <Button label="Abort" onClick={this.onHide} />
+      </div>
+    );
+
+    return (
+      <div className="content-section implementation">
+        <Dialog
+          header="Create new compliance process"
+          footer={footer}
+          visible={this.state.visibleCPDialog}
+          style={{ width: '80vw' }}
+          onHide={this.onHide}
+          maximizable
+        >
+          <div>
+            <label htmlFor="processName">Name</label>
+            <InputText id="processName" value={this.state.processName} onChange={(e) => this.setState({processName: e.target.value})}/>
+          </div>
+          <div>
+            <label htmlFor="processRule">Rule</label>
+            <InputText id="processRule" value={this.state.processRule} onChange={(e) => this.setState({processRule: e.target.value})}/>
+          </div>
+        </Dialog>
+      </div>
+    );
   }
 
   renderGraphPropsPanel(){
@@ -195,10 +245,10 @@ export default class Alternatives extends Component {
           <Button label="add req" onClick={this.addComplianceRequirement} />
           <br />
           <br />
-          <Button label="add pattern" onClick={this.addComplianceProcessPattern} />
+          <Button label="add pattern" onClick={this.showComplianceProcessPatternAddDialog} />
           <br />
           <br />
-          <Button label="add compliance process" onClick={this.addComplianceProcess} />
+          <Button label="add compliance process" onClick={this.showComplianceProcessAddDialog} />
         </div>
       </div>
     )
@@ -214,6 +264,7 @@ export default class Alternatives extends Component {
             position="topright"
         />
         <div>
+          {this.renderCPDialog()}
           <section className="container-graph">
             {this.renderGraphEditPanel()}
             <div className="viewer" id="alt-graph-container" />
