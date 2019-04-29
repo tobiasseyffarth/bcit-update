d6715c44de999178dacc8744392b0021783f5f96
Tobias Seyffarth <tobias.seyffarth@student.uni-halle.de>
2019-04-29 17:29:59 +0200

    ComplianceView zeigt auswirkungen in dialogen

--- src/view/container/ComplianceView.jsx
+++ src/view/container/ComplianceView.jsx
@@ -3,8 +3,6 @@ import { ListBox } from 'primereact/listbox';
 import { Button } from 'primereact/button';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Growl } from 'primereact/growl';
-import { Dialog } from 'primereact/dialog';
-import cytoscape from 'cytoscape';
 import RemoveDialog from './../dialog/RemoveDialog';
 import ChangeDialog from './../dialog/ChangeDialog';
 import '../../App.css';
@@ -12,7 +10,6 @@ import ProjectModel from '../../models/ProjectModel';
 import * as GraphQuery from '../../controller/graph/GraphQuery';
 import * as ComplianceQuery from './../../controller/compliance/ComplianceQuery';
 import * as AnalyzeChange from '../../controller/analyze/AnalyzeChange';
-import * as GraphRenderer from '../../controller/graph/GraphRenderer';
 
 class ComplianceView extends Component {
   constructor(props) {
@@ -24,11 +21,6 @@ class ComplianceView extends Component {
       selectedCompliance: null, // selected element in the listbox
       visibleRemove: false,
       visibleChange: false,
-      nodeId: null,
-      nodeName: null,
-      nodeType: null,
-      modelType: null,
-      nodeProps: [],
     };
 
     this.onHide = this.onHide.bind(this);
@@ -43,19 +35,6 @@ class ComplianceView extends Component {
   onHide() {
     this.setState({ visibleRemove: false });
     this.setState({ visibleChange: false });
-  }
-
-  hookGraphOnClick(graph){
-    const _this = this;
-
-    graph.on('click', (evt) => { // http://js.cytoscape.org/#core/events
-      const element = evt.target;
-      if (element === graph) { // background
-        _this.renderGraphProps(null);
-      } else if (element.isNode()) { // edge
-        _this.renderGraphProps(element);
-      }
-    });
   }
 
   getRemoveGraph(){
@@ -76,10 +55,7 @@ class ComplianceView extends Component {
       } else {
         if (deleteGraph !== null && deleteGraph.nodes().length > 1) {
           ProjectModel.setRemoveGraph(deleteGraph);
-          this.setState({visibleRemove: true}, () => {
-                // this.renderRemoveGraph(deleteGraph);
-              },
-          );
+          this.setState({visibleRemove: true});
         }
         if (deleteGraph !== null && deleteGraph.nodes().length <= 1) {
           const detail = 'no violations found';
@@ -107,10 +83,7 @@ class ComplianceView extends Component {
       } else {
         if (changeGraph !== null && changeGraph.nodes().length > 1) {
           ProjectModel.setChangeGraph(changeGraph);
-          this.setState({visibleChange: true}, () => {
-                // this.renderChangeGraph(changeGraph);
-              },
-          );
+          this.setState({visibleChange: true});
         }
         if (changeGraph !== null && changeGraph.nodes().length <= 1) {
           const detail = 'no demands found';
@@ -140,190 +113,6 @@ class ComplianceView extends Component {
     const reqText = ComplianceQuery.toString(compliance.requirement, id);
     this.setState({ selectedCompliance });
     this.setState({ complianceText: reqText });
-  }
-
-  renderRemoveGraph(graph) {
-    const containerRemove = document.getElementById('graph-container-remove');
-    const graphDelete = cytoscape({
-      container: containerRemove,
-      style: [ // the stylesheet for the graph
-        {
-          selector: 'node',
-          style: {
-            'background-color': 'white',
-            'border-style': 'solid',
-            'border-color': 'black',
-            'border-width': 1,
-            label: 'data(display_name)',
-            'font-size': 10,
-            'text-wrap': 'wrap',
-            'text-max-width': 20,
-            shape: 'rectangle',
-          },
-        },
-        {
-          selector: 'edge',
-          style: {
-            width: 1,
-            'line-color': '#666',
-            'mid-target-arrow-color': '#666',
-            'mid-target-arrow-shape': 'triangle',
-            'line-style': 'dotted',
-          },
-        },
-      ],
-      layout: {
-        name: 'grid',
-        rows: 1,
-      },
-    });
-
-    GraphRenderer.renderAnalyzeGraph(graphDelete, graph);
-    this.hookGraphOnClick(graphDelete);
-  }
-
-  renderChangeGraph(graph) {
-    const containerChange = document.getElementById('graph-container-change');
-    const graphChange = cytoscape({
-      container: containerChange,
-      style: [ // the stylesheet for the graph
-        {
-          selector: 'node',
-          style: {
-            'background-color': 'white',
-            'border-style': 'solid',
-            'border-color': 'black',
-            'border-width': 1,
-            label: 'data(display_name)',
-            'font-size': 10,
-            'text-wrap': 'wrap',
-            'text-max-width': 20,
-            shape: 'rectangle',
-          },
-        },
-        {
-          selector: 'edge',
-          style: {
-            width: 1,
-            'line-color': '#666',
-            'mid-target-arrow-color': '#666',
-            'mid-target-arrow-shape': 'triangle',
-            'line-style': 'dotted',
-          },
-        },
-      ],
-      layout: {
-        name: 'grid',
-        rows: 1,
-      },
-    });
-
-    GraphRenderer.renderAnalyzeGraph(graphChange, graph);
-    this.hookGraphOnClick(graphChange);
-  }
-
-  renderGraphPropsPanel() {
-    return (
-      <div className="property-panel">
-        <div>
-          <label>ID: {this.state.nodeId}</label>
-        </div>
-        <br />
-        <div>
-          <label>Name: {this.state.nodeName}</label>
-        </div>
-        <br />
-        <div>
-          <label>Node Type: {this.state.nodeType}</label>
-        </div>
-        <br />
-        <div>
-          <label>Model Type: {this.state.modelType}</label>
-        </div>
-        <br />
-        <div>
-          <ListBox
-            style={{ width: '100%' }}
-            options={this.state.nodeProps}
-            optionLabel="name"
-          />
-        </div>
-      </div>
-    );
-  }
-
-  renderGraphProps(node){
-    if (node !== null) {
-      this.setState({ nodeId: node.data('id') });
-      this.setState({ nodeName: node.data('name') });
-      this.setState({ nodeType: node.data('nodetype') });
-      this.setState({ modelType: node.data('modeltype') });
-
-      const nodeType = node.data('nodetype');
-      if (nodeType !== 'compliance'){ // non compliance nodes
-        this.setState({ nodeProps: node.data('props') });
-      } else { // compliance nodes
-        this.setState({ nodeProps: [] });
-      }
-    } else {
-      this.setState({ nodeId: null });
-      this.setState({ nodeName: null });
-      this.setState({ nodeType: null });
-      this.setState({ modelType: null });
-      this.setState({ nodeProps: [] });
-    }
-  }
-
-  renderRemoveDialog() {
-    const footer = (
-      <div>
-        <Button label="close" onClick={this.onHide} />
-      </div>
-    );
-
-    return (
-      <div className="content-section implementation">
-        <Dialog
-          header="Graph Remove"
-          footer={footer}
-          visible={this.state.visibleRemove}
-          style={{ width: '80vw' }}
-          onHide={this.onHide}
-          maximizable
-        >
-          <section className="container-graph">
-            <div className="viewer" id="graph-container-remove" />
-            {this.renderGraphPropsPanel()}
-          </section>
-        </Dialog>
-      </div>
-    );
-  }
-
-  renderChangeDialog() {
-    const footer = (
-      <div>
-        <Button label="close" onClick={this.onHide} />
-      </div>
-    );
-
-    return (
-      <div className="content-section implementation">
-        <Dialog
-          header="Graph Change"
-          footer={footer}
-          visible={this.state.visibleChange}
-          style={{ width: '80vw' }}
-          onHide={this.onHide}
-          maximizable
-        >
-          <section className="container-graph">
-            <div className="viewer" id="graph-container-change" />
-            {this.renderGraphPropsPanel()}
-          </section>
-        </Dialog>
-      </div>
-    );
   }
 
   renderComplianceSelector(){
@@ -396,8 +185,3 @@ class ComplianceView extends Component {
 }
 
 export default ComplianceView;
-
-/*
-{this.renderRemoveDialog()}
-{this.renderChangeDialog()}
-*/
